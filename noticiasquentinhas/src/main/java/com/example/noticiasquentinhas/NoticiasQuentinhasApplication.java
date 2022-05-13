dbababbbd367d94e0b89b79fa2938d12d146f349
monteiro141 <46536704+monteiro141@users.noreply.github.com>
2022-05-13 15:17:59 +0100

    created project

---
+++ noticiasquentinhas/src/main/java/com/example/noticiasquentinhas/NoticiasQuentinhasApplication.java
@@ -0,0 +1,13 @@
+package com.example.noticiasquentinhas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoticiasQuentinhasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoticiasQuentinhasApplication.class, args);
+    }
+
+}
---
+++ noticiasquentinhas/src/test/java/com/example/noticiasquentinhas/NoticiasQuentinhasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.noticiasquentinhas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoticiasQuentinhasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
