15bad2dff8f00f8092a5a288e5f0c1e592907491
monteiro141 <46536704+monteiro141@users.noreply.github.com>
2022-05-21 22:45:12 +0100

    Updated create and edit

--- noticiasquentinhas/src/main/java/com/example/noticiasquentinhas/controllers/publisherController.java
+++ noticiasquentinhas/src/main/java/com/example/noticiasquentinhas/controllers/publisherController.java
@@ -9,15 +9,13 @@ import com.example.noticiasquentinhas.service.NewsService;
 import com.example.noticiasquentinhas.service.TopicService;
 import com.example.noticiasquentinhas.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.MalformedURLException;
 import java.util.ArrayList;
@@ -68,20 +66,23 @@ public class publisherController {
     }
 
     @GetMapping(path = "/publisher/addTopic")
-    public String returnToPublisherAddTopic(Model model) throws MalformedURLException {
+    public String returnToPublisherAddTopic(@RequestParam(name="create", required = false, defaultValue = "none") String name, Model model) throws MalformedURLException {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         model.addAttribute("loggedInUser",(userService.currentUserName(authentication.getName())));
         model.addAttribute("linkPath","addTopic");
         model.addAttribute("addTopic",new TopicForm());
+        model.addAttribute("create",name);
         return "publisher/index";
     }
 
-    @PostMapping(path = "/publisher/addTopic")
+    @PostMapping(path = "/publisher/addTopicButton")
     public String creatingANews(@ModelAttribute("addTopic") TopicForm topicForm){
-        System.out.println("Topic:");
-        System.out.println(topicForm.getName());
-        topicService.save(topicForm);
-        return "redirect:/";
+        try{
+            topicService.save(topicForm);
+        } catch (Exception e){
+            return "redirect:/publisher/addTopic?create=fail";
+        }
+        return "redirect:/publisher/addTopic?create=success";
     }
 
     @GetMapping(path = "/publisher/searchTopic")
@@ -94,25 +95,27 @@ public class publisherController {
     }
 
     @GetMapping(path = "/publisher/createNews")
-    public String returnToPublisherCreateNews(Model model) throws MalformedURLException {
+    public String returnToPublisherCreateNews(@RequestParam(name="create", required = false, defaultValue = "none") String name, Model model) throws MalformedURLException {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         model.addAttribute("loggedInUser",(userService.currentUserName(authentication.getName())));
         model.addAttribute("linkPath","createNews");
         model.addAttribute("newsForm", new NewsForm());
         model.addAttribute("topicsList",topicService.topicsList());
+        model.addAttribute("create",name);
         return "publisher/index";
     }
 
-    @PostMapping("/publisher/createNews")
+    @PostMapping("/publisher/createNewsButton")
     public String creatingANews(@ModelAttribute("newsForm") NewsForm newsForm){
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        System.out.println("Newsform");
-        System.out.println(newsForm);
-        newsService.save(newsForm,
-                userService.search(authentication.getName()),
-                topicService.search(newsForm.getTopic()));
-
-        return "redirect:/";
+        try{
+            newsService.save(newsForm,
+                    userService.search(authentication.getName()),
+                    topicService.search(newsForm.getTopic()));
+        } catch (Exception e){
+            return "redirect:/publisher/createNews?create=fail";
+        }
+        return "redirect:/publisher/createNews?create=success";
     }
 
     @GetMapping(path = "/publisher/listNews")
@@ -126,11 +129,11 @@ public class publisherController {
     }
 
     @GetMapping(path = "/publisher/editNews/{id}")
-    public String returnToPublisherEditNews(@PathVariable(value="id") Integer id,Model model) throws MalformedURLException {
+    public String returnToPublisherEditNews(@RequestParam(name="edit", required = false, defaultValue = "none") String name, @PathVariable(value="id") Integer id,Model model) throws MalformedURLException {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         model.addAttribute("loggedInUser",(userService.currentUserName(authentication.getName())));
         model.addAttribute("linkPath","editNews");
-
+        model.addAttribute("edit",name);
         Optional<News> optional = newsService.findNew(id);
         News newsEdit = null;
         if(optional.isPresent()){
@@ -140,9 +143,14 @@ public class publisherController {
         return "publisher/index";
     }
 
-    @PostMapping("/publisher/editNews")
+    @PostMapping("/publisher/editNewsButton")
     public String saveNew(@ModelAttribute("theNew") News news){
         newsService.saveEditNew(news);
+        try{
+            newsService.saveEditNew(news);
+        } catch (Exception e){
+            return "redirect:/publisher/editNews/"+news.getNews_id()+"?edit=fail";
+        }
         return "redirect:/";
     }
 }
