df31070dfed83eb85f45acaaee5b59a0fbbe0e31
Brunex0 <75696611+Brunex0@users.noreply.github.com>
2022-05-21 19:35:12 +0100

    add dynamic news to subscriber

--- noticiasquentinhas/src/main/java/com/example/noticiasquentinhas/controllers/subscriberController.java
+++ noticiasquentinhas/src/main/java/com/example/noticiasquentinhas/controllers/subscriberController.java
@@ -1,5 +1,8 @@
 package com.example.noticiasquentinhas.controllers;
 
+import com.example.noticiasquentinhas.entities.News;
+import com.example.noticiasquentinhas.service.NewsService;
+import com.example.noticiasquentinhas.service.TopicService;
 import com.example.noticiasquentinhas.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
@@ -10,6 +13,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.net.MalformedURLException;
+import java.util.ArrayList;
 
 @Controller
 public class subscriberController {
@@ -17,13 +21,26 @@ public class subscriberController {
     @Autowired
     private UserService userService;
 
+    private NewsService newsService;
+
+    public subscriberController(UserService userService, NewsService newsService, TopicService topicService) {
+        this.userService = userService;
+        this.newsService = newsService;
+    }
+
+
+
     @GetMapping(path = "/subscriber/")
     public String returnToSubscriberIndex(Model model) throws MalformedURLException {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         model.addAttribute("loggedInUser",(userService.currentUserName(authentication.getName())));
         model.addAttribute("linkPath","home");
+        ArrayList<News> newsList = publisherController.lastTenNews(newsService.listAllNews());
+        model.addAttribute("newsList",newsList);
         return "subscriber/index";
     }
+
+
 
     @GetMapping("/subscriber/topic")
     public String returnToSubscriberTopic(Model model) throws MalformedURLException {
